d10085bcbd1ad66d9d579857e7aec4bc2c9fdc54
Dave Swebilius <dswebilius@gmail.com>
2018-01-17 01:03:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ['Milo','Otis','Garfield']
 
-function destructivelyAppendKittens (name) {
+function destructivelyAppendKitten (name) {
     kittens.push (`${name}`);
     return kittens;
 }
